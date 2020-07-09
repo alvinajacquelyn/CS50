a43e6fb69151ec0aa90711a8c931f3b3793ad25c
alvinajacquelyn <alvinalopx@gmail.com>
2020-07-09 09:12:07 +0000

    algo

--- selectionsort.c
+++ selectionsort.c
@@ -1,7 +1,51 @@
-// First, we’ll look at each number, and remember the smallest one we’ve seen. Then, we can swap it with the first number in our list, since we know it’s the smallest:
-// Now we know at least the first element of our list is in the right place, so we can look for the smallest element among the rest, and swap it with the next unsorted element (now the second element):
-// We can repeat this over and over, until we have a sorted list.
 //This algorithm is called selection sort, and we might write pseudocode like this:
 // For i from 0 to n–1
 //     Find smallest item between i'th item and last item
 //     Swap smallest item with i'th item
+
+//smallest element is exchanged with the first element of the unsorted list of elements
+//(the exchanged element takes the place where smallest element is initially placed).
+//then the second smallest element is exchanged with the second element of the unsorted list of elements and so on until all the elements are sorted
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Here i & j for loop counters,
+    //temp for swapping,
+    //count for total number of elements aka n,
+    //number[] to store the input numbers in array.
+    // You can increase or decrease the size of number array as per requirement
+
+    int i, j , temp, count, number[30];
+    printf("How many numbers are u going to enter?: ");
+    scanf("%d",&count);
+
+    for ( i=0; i< count; i ++) //loop to get elements sorted in array
+    {
+        scanf("%d", &number[i]);
+    }
+
+    // Logic of selection sort algorithm
+    for (i=0; i< count; i++)
+    {
+        for (j= i+1; j< count; j++)
+        {
+            if (number[i] > number[j])
+            {
+                temp = number[i];
+                number[i] = number[j];
+                number[j] = temp; //swap
+            }
+        }
+    }
+
+   printf("Sorted elements: ");
+   for(i=0 ; i<count ; i++)
+   {
+      printf(" %d",number[i]);
+   }
+   printf("\n");
+}
